844127ab21f160d28391ff1e1127c9929df8903a
thanabat <thanabat@odds.team>
2022-08-20 10:25:14 +0700

    2 files modified

--- App.tsx
+++ App.tsx
@@ -5,8 +5,6 @@ export default function App() {
   return (
     <div>
       <h1>Hello React!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <p>Hello Hello hed eyang yoo noh emai</p>
     </div>
   );
 }
