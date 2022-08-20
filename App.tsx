d2fe02d3e2acf719699229468b4d6953478fa0c6
thanabat <thanabat@odds.team>
2022-08-20 09:45:51 +0700

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <p>Hello Hello hed eyang yoo noh emai</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-b8mpz8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-b8mpz8)
